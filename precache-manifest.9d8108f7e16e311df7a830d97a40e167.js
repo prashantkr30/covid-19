1c1de7b3ed4a3c07423325fa91d63ac1a275969f
Prashant Kumar <reprashant3006@gmail.com>
2020-05-07 18:16:25 +0530

    Updates

---
+++ precache-manifest.9d8108f7e16e311df7a830d97a40e167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c059dd388938d22a3050371e52d391e",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "03dcca55e9d219532fe7",
+    "url": "/covid-19/static/css/main.22d22c20.chunk.css"
+  },
+  {
+    "revision": "37b30033ffdd0dc87441",
+    "url": "/covid-19/static/js/2.5caf3615.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.5caf3615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dcca55e9d219532fe7",
+    "url": "/covid-19/static/js/main.da3ad0d6.chunk.js"
+  },
+  {
+    "revision": "aed3bca2f28f6f49e7ae",
+    "url": "/covid-19/static/js/runtime-main.56b34229.js"
+  },
+  {
+    "revision": "f3e6fbe5624e22a20026e2c7323c6263",
+    "url": "/covid-19/static/media/image.f3e6fbe5.png"
+  }
+]);
